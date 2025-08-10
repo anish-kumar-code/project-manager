45d43382b08fb0aeda1453f1324d439c9f6665d8
Anish <anishkr2842003@gmail.com>
2025-08-10 21:18:44 +0530

    cors error solve

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -85,6 +85,7 @@ const loginUser = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
+        sameSite: 'none',
     };
 
     return res
@@ -110,6 +111,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
+        sameSite: 'none',
     };
 
     return res
